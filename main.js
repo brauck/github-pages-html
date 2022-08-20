9d2907a2070c9743781b5ad109ee37c5b867067a
brauck <109923715+brauck@users.noreply.github.com>
2022-08-20 19:33:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const body = document.querySelector('body');
-window.addEventListener("click", () => body.style.backgroundColor = 'blue');
+window.addEventListener("click", () => {
+  const status = body.getAttribute("status");
+  if (status === "false") {
+    body.style.backgroundColor = 'red';
+    body.setAttribute("status"."true");
+  }
+  else {
+    body.style.backgroundColor = 'green';
+    body.setAttribute("status"."false");
+  }
+});
